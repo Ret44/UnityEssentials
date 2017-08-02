1d709dd141cb7d381acadd52d535dbee30fe07bd
Bartłomiej Sieczka <retro44@gmail.com>
2017-08-03 00:53:25 +0200

    SingletonBehaviour script

---
+++ Essentials/SingletonBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBehaviour<T> : MonoBehaviour {
+
+    public static T instance;
+
+    public void Awake()
+    {
+        if (instance != null)
+            Debug.LogWarningFormat("Multiple instances of {0} singleton!", (typeof(T)).ToString());
+
+        T[] objs = FindObjectsOfType(typeof(T)) as T[];
+        instance = objs[0];
+    }
+
+    public void OnDestroy()
+    {
+        instance = default(T);
+    }
+}
